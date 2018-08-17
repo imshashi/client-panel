123af0832d933e2464ebf5ba5274bc150c765ffb
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-17 23:58:45 +0530

    add form to AddClient component

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,10 +1,96 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class AddClient extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: ''
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div>
-        <h1>Add Client</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn btn-link">
+              <i className="fas fa-arrow-circle-left"></i> Back To Dashboard
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">Add Client</div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="firstName"
+                  minLength="2"
+                  required
+                  onChange={ this.onChange }
+                  value={ this.state.firstName }
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="firstName">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="lastName"
+                  minLength="2"
+                  required
+                  onChange={ this.onChange }
+                  value={ this.state.lastName }
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="firstName">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  minLength="2"
+                  required
+                  onChange={ this.onChange }
+                  value={ this.state.email }
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="firstName">Phone</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="phone"
+                  minLength="2"
+                  required
+                  onChange={ this.onChange }
+                  value={ this.state.phone }
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="firstName">Balance</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="balance"
+                  minLength="2"
+                  required
+                  onChange={ this.onChange }
+                  value={ this.state.balance }
+                />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
