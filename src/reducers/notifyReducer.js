9266d006741a67a3a913948a0193702256e8e4e2
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-22 23:35:01 +0530

    fix typo

--- src/reducers/notifyReducer.js
+++ src/reducers/notifyReducer.js
@@ -5,7 +5,7 @@ const initialState = {
   messageType: null
 }
 
-export default = (state=initialState, action) => {
+export default function(state=initialState, action) {
   switch(action.type) {
     case NOTIFY_USER:
       return {
